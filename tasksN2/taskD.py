3d397155b00056e1579eacd977b9cb6f5b66a956
AndrewRomanyshyn <andrewtheere@gmail.com>
2023-07-31 10:33:50 +0200

    modified taskD

--- tasksN2/taskD.py
+++ tasksN2/taskD.py
@@ -9,25 +9,25 @@ import asyncio
 import time
 
 
-def get_tasks(session):
-    tasks = []  # список запросів
-    url = 'http://httpbin.org/delay/3'
-    for i in range(1, 100):
-        tasks.append(session.get(url=url, ssl=False))  # добавляємо в список запроси (таски)
-    return tasks
+async def get_request(session):
+    async with session.get('http://httpbin.org/delay/3') as response:
+        return await response.text()
 
 
-async def request_asynchronious():  # робимо корутину( тобто асинхронну функцію, задачу)
-    async with aiohttp.ClientSession() as session:  # сессія для того щоб відправляти запроси
-        tasks = get_tasks(session)  # приймаємо список запросів
-        responses = await asyncio.gather(*tasks)  # тут список наших запросів які ми відсилаємо
-        for response in responses:
-            print(response)
+async def get_multiple_requests():
+    async with aiohttp.ClientSession() as session:
+        tasks = [get_request(session) for _ in range(100)]
+        return await asyncio.gather(*tasks)
 
 
-start = time.time()
-asyncio.run(request_asynchronious())  # event loop
-end = time.time()
-print(start - end)
+async def main():
+    responses = await get_multiple_requests()
+    for response in responses:
+        print(response)
 
 
+if __name__ == "__main__":
+    start = time.time()
+    asyncio.run(main())
+    end = time.time()
+    print("Time taken:", end - start)
